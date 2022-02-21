dac24f943948b194e2b1b5009771d110e6ccf163
Kuang Hsu <kuang@localhost.localdomain>
2022-02-21 08:44:22 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
 git clone --depth 1 https://github.com/wbthomason/packer.nvim  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
+sh nerd-fonts/install.sh
+rm -rf nerd-fonts
+
